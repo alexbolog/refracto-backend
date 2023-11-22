da728a649deccad686189d22eb8422591ab2783b
alexbolog <mbolog.alex@gmail.com>
2023-11-22 16:48:09 +0200

    cleanup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -24,35 +24,4 @@ export class AppController {
     const projectIds = await this.supabaseService.getProjectIds();
     return await this.crowdfundingScService.getProjectDetails(projectIds);
   }
-
-  //   @Get('/projects/:id')
-  //   async getProjectById(@Param('id') id: number): Promise<any> {
-  //     const projects = await this.getProjects();
-  //     console.log('Fetching project with id', id);
-  //     const foundProject = projects.find(
-  //       (p: any) => p.project_id.toNumber() === id,
-  //     );
-  //     console.log(projects, foundProject);
-  //     return foundProject;
-  //   }
-
-  @Post('/projects/update')
-  async updateProjects(): Promise<any> {
-    const projectIds = await this.supabaseService.getProjectIds();
-    const projectDetails = await this.crowdfundingScService.getProjectDetails(
-      projectIds,
-    );
-
-    for (const project of projectDetails) {
-      const holdersCount = await this.apiService.queryTokenHolders(
-        REFRACTO_LOAN_SHARE_TOKEN_ID,
-        project.share_token_nonce,
-      );
-      await this.supabaseService.updateProjectScProgress(
-        project.project_id,
-        project.cf_progress,
-        holdersCount - 1,
-      );
-    }
-  }
 }
