cb595842458bd8f4914efb1dc1e67e49aa3f16f6
alexbolog <mbolog.alex@gmail.com>
2024-01-30 13:01:51 +0200

    added status field to supabase service update project fn

--- src/common/supabase.service.ts
+++ src/common/supabase.service.ts
@@ -59,12 +59,14 @@ export class SupabaseService {
     crowdfundedAmount: number,
     holdersCount: number,
     shareTokenNonce: number,
+    status: number,
   ): Promise<void> {
     const { error } = await this.supabase.rpc('update_project_sc_data', {
       _projectid: projectId,
       _amount: crowdfundedAmount,
       _holderscount: holdersCount,
       _sharetokennonce: shareTokenNonce,
+      _status: status,
     });
     if (error) {
       console.log(
