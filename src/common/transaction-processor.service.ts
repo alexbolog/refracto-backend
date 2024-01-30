10f7a4a27f24f424429032fe685d806ec33b0a03
alexbolog <mbolog.alex@gmail.com>
2024-01-30 16:44:27 +0200

    extracted async fn parseTransaction

--- src/common/transaction-processor.service.ts
+++ src/common/transaction-processor.service.ts
@@ -3,33 +3,23 @@ import BigNumber from 'bignumber.js';
 
 import dotenv = require('dotenv');
 import { ProcessedTransaction, TransactionType } from 'src/models';
+import { SupabaseService } from './supabase.service';
 
 dotenv.config();
 
 @Injectable()
 export class TransactionProcessorService {
-  constructor(private readonly logger: Logger) {}
+  constructor(private readonly supabaseService: SupabaseService) {}
 
   async processTransactions(
     transactions: TransactionType[],
   ): Promise<ProcessedTransaction[]> {
-    const processedTransactions = transactions.map((transaction) => {
-      const projectId = this.extractProjectId(transaction.action);
-      const transferInfo =
-        transaction.function === 'invest'
-          ? this.extractTransferInfo(transaction.action, 'USDC')
-          : null;
-      return {
-        tx_hash: transaction.txHash,
-        sender: transaction.sender,
-        function: transaction.function,
-        tx_timestamp: transaction.timestamp,
-        status: transaction.status,
-        amount: transferInfo?.amount ?? 0,
-        transfer_token: transferInfo?.transferToken ?? 'N/A',
-        project_id: projectId,
-      };
-    });
+    const processedTransactions = [];
+
+    for (const transaction of transactions) {
+      const processedTransaction = await this.parseTransaction(transaction);
+      processedTransactions.push(processedTransaction);
+    }
 
     return processedTransactions;
   }
@@ -55,5 +45,25 @@ export class TransactionProcessorService {
       return null;
     }
     return parseInt(txAction.arguments.functionArgs, 16);
+  }
+
+  private async parseTransaction(
+    transaction: TransactionType,
+  ): Promise<ProcessedTransaction> {
+    const projectId = this.extractProjectId(transaction.action);
+    const transferInfo =
+      transaction.function === 'invest'
+        ? this.extractTransferInfo(transaction.action, 'USDC')
+        : null;
+    return {
+      tx_hash: transaction.txHash,
+      sender: transaction.sender,
+      function: transaction.function,
+      tx_timestamp: transaction.timestamp,
+      status: transaction.status,
+      amount: transferInfo?.amount ?? 0,
+      transfer_token: transferInfo?.transferToken ?? 'N/A',
+      project_id: projectId,
+    };
   }
 }
